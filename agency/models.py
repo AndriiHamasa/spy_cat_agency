5dd169bc48653881ec1119a2779aa028fa18b00c
Andrii <andrii.khamaza0@gmail.com>
2024-11-26 18:41:52 +0300

    add models

--- agency/models.py
+++ agency/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
+class Cat(models.Model):
+    name = models.CharField(max_length=100)
+    years = models.IntegerField()
+    breed = models.CharField(max_length=100)
+    salary = models.IntegerField()
+
+
+class Mission(models.Model):
+    cat = models.ForeignKey(Cat, on_delete=models.CASCADE, related_name="Missions")
+    is_completed = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Target(models.Model):
+    name = models.CharField(max_length=100)
+    country = models.CharField(max_length=100)
+    notes = models.TextField()
+    is_complete = models.BooleanField(default=False)
+    mission = models.ForeignKey(Mission, on_delete=models.CASCADE, related_name="targets")
